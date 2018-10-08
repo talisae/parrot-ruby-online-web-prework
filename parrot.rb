b2cc4eae1b49fa91450387c1801f315a26f43c98
Talisa Erazo <talisaerazo@gmail.com>
2018-10-08 20:17:14 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot (phrase, phrase = "Squawk!")
+  puts "Pretty bird!"
+    return "Pretty bird!"
+  puts "Squawk!"
+    return "Squawk!"
+end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
